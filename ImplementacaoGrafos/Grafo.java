00ceb89230d40552716bd8f611d771b14f4e29d3
Andre <Andre@Andre-PC>
2018-06-14 20:02:10 -0300

    correcao de erro de acesso a posicao matriz

--- ImplementacaoGrafos/Grafo.java
+++ ImplementacaoGrafos/Grafo.java
@@ -5,14 +5,14 @@ public class Grafo {
 	int[][] M;
 	public Grafo(String arestas) {
 		this.arestas = arestas;
-		M = new int[n][m];
 		obterDimensaoMatriz(this.arestas);
+		M = new int[n][m];
 	}
 	
 	private void obterDimensaoMatriz(String arestas) {
 		int qntArestas = 0;
 		//de acordo com a quantidade de chaves abertas no arquivo, se tem a quantidade de arestas do grafo que serão as dimensões da matriz
-		for(int i = 0; i<arestas.length(); i++)
+		for(int i = 0; i < arestas.length(); i++)
 			if(arestas.charAt(i) == '{') {
 				qntArestas++;
 				n = qntArestas;
--- ImplementacaoGrafos/MatrizAdjacencia.java
+++ ImplementacaoGrafos/MatrizAdjacencia.java
@@ -6,15 +6,15 @@ public class MatrizAdjacencia {
 		//for(int i = 1; i<= G.m; i++) {
 			for(int j = 0; j < G.arestas.length(); j++) {
 				//se o que foi lido do arquivo não for esses caracteres então será a aresta do grafo
-				if(G.arestas.charAt(j) != '{' || G.arestas.charAt(j) != '}' || G.arestas.charAt(j) != ',' || G.arestas.charAt(j) != ' ') {
-					u = G.arestas.charAt(j);
+				if(Character.isDigit(G.arestas.charAt(j))) { 
+						//|| G.arestas.charAt(j) == '}' || G.arestas.charAt(j) != ',' || G.arestas.charAt(j) != ' ') {
+					u = Character.getNumericValue(G.arestas.charAt(j));
 					j = j+2; 
-					v = G.arestas.charAt(j);
-					G.M[u][v] = 1; 
-					G.M[v][u] = 1;
+					v = Character.getNumericValue(G.arestas.charAt(j));
+					G.M[u-1][v-1] = 1; //foi preciso subtrair um pois o array jo java começa a contar a partir do indice 0
+					G.M[v-1][u-1] = 1;
 				}
 			}
 		//}
-		
 	}
 }
